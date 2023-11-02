e88e272873d477828260b867a120477b2e338631
Jenipher Oduor <jenipheroduor@gmail.com>
2023-11-02 11:47:13 +0300

    Initial Commit

---
+++ app/migrations/versions/47db06d3da21_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 47db06d3da21
+Revises: 
+Create Date: 2023-11-02 11:31:53.325268
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '47db06d3da21'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
